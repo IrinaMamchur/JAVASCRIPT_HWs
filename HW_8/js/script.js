df1614e41bf29f1d229ca6e1f709335a5e0e85b0
IrinaMamchur <shevchenko_irina@meta.ua>
2019-03-28 20:56:36 +0200

    new HWs

---
+++ HW_8/js/script.js
@@ -0,0 +1,85 @@
+"use strict";
+
+const galleryItems = [
+  {
+    preview: 'img/preview-1.jpeg',
+    fullview: 'img/fullview-1.jpeg',
+    alt: 'alt text 1'
+  },
+  {
+    preview: 'img/preview-2.jpeg',
+    fullview: 'img/fullview-2.jpeg',
+    alt: 'alt text 2'
+  },
+  {
+    preview: 'img/preview-3.jpeg',
+    fullview: 'img/fullview-3.jpeg',
+    alt: 'alt text 3'
+  },
+  {
+    preview: 'img/preview-4.jpeg',
+    fullview: 'img/fullview-4.jpeg',
+    alt: 'alt text 4'
+  },
+  {
+    preview: 'img/preview-5.jpeg',
+    fullview: 'img/fullview-5.jpeg',
+    alt: 'alt text 5'
+  },
+  {
+    preview: 'img/preview-6.jpeg',
+    fullview: 'img/fullview-6.jpeg',
+    alt: 'alt text 6'
+  }
+];
+
+const gallery = document.querySelector('.image-gallery');
+const fullView = document.createElement('div');
+const bigImg = document.createElement('img');
+let fullviews = galleryItems[0].fullview;
+const alt = galleryItems[0].alt;
+bigImg.classList.add('fullview');
+bigImg.setAttribute('src', fullviews);
+bigImg.setAttribute('alt', alt);
+gallery.append(bigImg);
+
+const ul = document.createElement('ul');
+
+function createImg(array) {
+  array.map(item => {
+    const li = document.createElement('li');
+    const imgEl = document.createElement('img');
+    ul.classList.add('preview');
+    imgEl.classList.add('img-list');
+    li.classList.add('js-list-item');
+    imgEl.setAttribute('src', item.preview);
+    imgEl.setAttribute('data-fullview', item.fullview);
+    imgEl.setAttribute('alt', item.alt);
+    li.append(imgEl);
+    ul.append(li);
+    return li;
+  });
+  return ul;
+}
+gallery.append(ul);
+
+const fullGallery = createImg(galleryItems);
+gallery.append(fullGallery);
+
+gallery.addEventListener('click', clickImg);
+
+function clickImg() {
+  const name = event.target;
+  if (name.nodeName !== 'IMG') return;
+  addClass(name);
+  let srcImg = event.target.dataset.fullview;
+  let newImg = document.querySelector('.fullview');
+(newImg.src = srcImg);
+}
+function addClass(image) {
+  const currentImg = ul.querySelector('img.active');
+  if (currentImg) {
+    currentImg.classList.remove('active');
+  }
+  image.classList.add('active');
+}
---
+++ HW_9/js/script.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const btnStart = document.querySelector('.js-start');
+const timeClock = document.querySelector('.js-time');
+const btnReset = document.querySelector('.js-reset');
+const btnlap = document.querySelector('.js-take-lap');
+const ul = document.querySelector('.js-laps');
+
+const arr = [];
+
+const timer = {
+  startTime: null,
+  deltaTime: null,
+  pauseTime: null,
+  id: null,
+
+  isActive: false,
+  startClock(upDate) {
+    if (!this.isActive) {
+      this.isActive = true;
+      this.startTime = Date.now();
+      this.id = setInterval(() => {
+        let currentTime = Date.now();
+        this.deltaTime = currentTime - this.startTime + this.pauseTime;
+        upDate(this.deltaTime);
+      }, 100);
+
+      if (this.isActive === true) {
+        pause(this.pauseTime);
+        this.pauseTime = this.deltaTime;
+        this.isActive = true;
+      }
+    } else {
+      this.startTime = this.deltaTime + Date.now();
+      onContinue(this.deltaTime);
+      clearInterval(this.id);
+      this.isActive = false;
+    }
+  },
+  resetClock(upDate) {
+    this.isActive = false;
+    clearInterval(this.id);
+    this.startTime = null;
+    this.id = null;
+    this.pauseTime = null;
+    this.deltaTime = null;
+    upDate(this.deltaTime);
+    onStart(this.deltaTime);
+    ul.innerHTML = '';
+  },
+  onLap() {
+    arr.push(this.deltaTime);
+    onArr();
+  }
+};
+
+let time = timer.startTime;
+
+function getFormattedTime(time) {
+  const setMin = Math.floor((time / 1000 / 60) % 60);
+  const Min = setMin >= 10 ? setMin : `0${setMin}`;
+  const setSec = Math.floor((time / 1000) % 60);
+  const Sec = setSec >= 10 ? setSec : `0${setSec}`;
+  const setMillisec = Math.floor((time / 100) % 10);
+  return `${Min}:${Sec}.${setMillisec}`;
+}
+
+function upDateClock(time) {
+  timeClock.textContent = getFormattedTime(time);
+}
+
+function pause() {
+  btnStart.textContent = 'Pause';
+}
+
+function onContinue() {
+  btnStart.textContent = 'Continue..';
+}
+
+function onStart() {
+  btnStart.textContent = 'Start';
+}
+
+function onArr() {
+  const li = document.createElement('li');
+  for (let el of arr) {
+    let list = getFormattedTime(el);
+    li.textContent = list;
+    ul.append(li);
+  }
+}
+btnStart.addEventListener('click', timer.startClock.bind(timer, upDateClock, pause, onContinue, onStart));
+btnReset.addEventListener('click', timer.resetClock.bind(timer, upDateClock));
+btnlap.addEventListener('click', timer.onLap.bind(timer, onArr));
